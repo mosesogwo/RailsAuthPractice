602460eb381c64d2f73ea9ac34b05de36c258d8f
Moses Ogwo <mosesogwo@gmail.com>
2019-07-31 06:10:07 +0100

    fix errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:user_id]
-      @current_user ||= User.find(session[:id])
+      @current_user ||= User.find(session[:user_id])
     else
       @current_user = nil
     end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :users
-  resources : sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
