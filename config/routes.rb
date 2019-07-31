69b4e6f8680b3fbca48fdc0a4f07e59a9d755760
Moses Ogwo <mosesogwo@gmail.com>
2019-07-31 05:54:26 +0100

    modify home view content

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  resources :users
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
   root 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+  resources : sessions, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
